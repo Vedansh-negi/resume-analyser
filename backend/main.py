cd0802473ef52d6e65b5228b245d7c4235cb690f
Vedansh-negi <vedanshbsnegi@gmail.com>
2026-03-25 07:58:12 +0530

    Refactor subprocess handling in main.py

--- backend/main.py
+++ backend/main.py
@@ -4,10 +4,8 @@ from pydantic import BaseModel
 import subprocess
 import json
 import os
-
 base_dir = os.path.dirname(__file__)
 cpp_path = os.path.join(base_dir, "/Users/vedanshnegi/Desktop/resume-optimizer/cpp_engine/analyzer")
-
 process = subprocess.Popen(
     [cpp_path],
     stdin=subprocess.PIPE,
@@ -15,10 +13,7 @@ process = subprocess.Popen(
     stderr=subprocess.PIPE,
     text=True
 )
-# ✅ THIS LINE IS REQUIRED
 app = FastAPI()
-
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -26,18 +21,14 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 class RequestData(BaseModel):
     resume: str
     job: str
-
 @app.get("/")
 def home():
     return {"message": "Resume Analyzer API Running"}
-
 @app.post("/analyze")
 def analyze(data: RequestData):
-
     process = subprocess.Popen(
         ["../cpp_engine/analyzer"],
         stdin=subprocess.PIPE,
@@ -45,11 +36,8 @@ def analyze(data: RequestData):
         stderr=subprocess.PIPE,
         text=True
     )
-
     input_data = data.resume + "\n" + data.job + "\n"
-
     output, error = process.communicate(input_data)
-
     return {
     "output": output,
     "error": error
